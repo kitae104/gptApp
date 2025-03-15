62ca4a404f6a49f121ea646f68093c2bd5125c99
Kitae's Github <aqua0405@gmail.com>
2025-03-16 00:31:58 +0900

    init

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>한글 가능 여부 테스트</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
---
+++ README.md
@@ -0,0 +1,8 @@
+### 시작하기 
+- npx expo start
+
+### Navigation 설치 
+- npm install @react-navigation/native
+- npx expo install react-native-screens react-native-safe-area-context
+
+npm install @react-navigation/bottom-tabs
